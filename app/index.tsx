f63acf6eb7762aabc26810499195e686b1c1066a
ssanti04072008 <ssanti04072008@gmail.com>
2025-10-01 16:14:54 +0000

    💄ui(index): Asignar color de fondo para la aplicacion

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import "@/global.css";
+import {Text, View} from "react-native";
+export default function Index(){
+    return(
+        <View className="">
+            <Text>wasa</Text>
+        </View>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
